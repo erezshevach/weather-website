df37b96d08abeef65d553d4b38aed90a9a447d1f
Erez <erez.shevach@gmail.com>
2021-12-26 18:26:33 +0200

    removing unnecessary comments

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const forecast = require('./utils/forecast')
 const app = express()
 const port = process.env.PORT || 3000
 
-
+   
 // define paths for Express config
 const publicDirPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
